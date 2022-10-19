f02cb44b8007e87525d37e81f6679380d7417704
jendos <huiartman@gmail.com>
2022-10-19 14:30:57 +0300

    models create

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self):
+        return self.name
