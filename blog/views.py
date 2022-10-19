9332bf5e40f021de95d2133a1618f4478d3c8416
jendos <huiartman@gmail.com>
2022-10-19 15:13:59 +0300

    index create

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>TEST</h1>')
+    return render(request, 'blog/index.html')
