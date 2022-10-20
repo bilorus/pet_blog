cc8f7688b37c9b0384f5d42744e8e5d3bbc62467
jendos <huiartman@gmail.com>
2022-10-20 10:40:15 +0300

    templates create

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'time_create', 'is_published']
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'text']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from blog.views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('page/', page)
 ]
